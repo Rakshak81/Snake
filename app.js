b12513e9d7274fbf3921b127f1d31e5adf09b098
Rakshak <rakshakshetty@Rakshaks-MacBook-Pro.local>
2022-07-02 15:21:20 +0100

    logic

--- app.js
+++ app.js
@@ -11,5 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
     let direction = 1
     let score = 0
     let speed = 0.9
+    let intervalTime = 0
     let interval = 0
-})
+
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
+
+        if(e.keycode === 39) {
+            direction = 1 // if we press the right arrow key the snake will go right one
+        } else if (e.keycode === 38) {
+            direction = -width // if we press the up arrow key the snake will go back 10 divs appearing to go up
+        } else if (e.keycode === 37) {
+            direction = -1  // if we press left the snake will go left one div
+        } else if (e.keycode ===40) {
+            direction = +width  // if we press the up down key the snake head will instantly appear in the div 10 divs from where you are now
+        }
+
+    }
+   })
+
